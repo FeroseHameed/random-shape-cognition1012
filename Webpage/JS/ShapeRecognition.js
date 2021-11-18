d323af314f50014ad7c99dbb0bea182f8050f952
yuaefimoff <92180203+yuaefimoff@users.noreply.github.com>
2021-11-18 00:52:11 -0500

    Nov 18 Changes
    Created base functionality for start page.
    - Prompt for username on window load
    - Add the menu buttons using jQuery
    - Play BGM on load and on loop

--- Webpage/JS/ShapeRecognition.js
+++ Webpage/JS/ShapeRecognition.js
@@ -1,1 +1,27 @@
+const buttons = ["Start", "Continue", "How to Play", "Settings"];
+var userName;
+var audio = new Audio("bgm.mp3");
+audio.loop=true;
 
+
+//Prepare GAME MENU when window loaded
+window.onload = function(){
+    //FUNCTIONAL REQUIREMENT: Prompt for a username
+    userName = prompt("Username:", "");
+
+    //FUNCTIONAL REQUIREMENT: Audio
+    audio.play();
+
+    //Main Buttons
+    for(var i = 1; i <= 4; i++){
+       var btn = document.createElement("button");
+ 
+       $(btn).html(buttons[i-1]);
+       $(btn).attr("id", "btn" +  i);
+
+       //May need onclick function set here
+ 
+       $("nav").append(btn);
+    }
+
+ };
