ce8dc7ac09288231d68ce22333ca77744e020967
FeroseHameed <61104469+FeroseHameed@users.noreply.github.com>
2021-11-28 13:44:52 -0500

    Update ShapeRecognition_server.js

--- Webpage/JS/ShapeRecognition_server.js
+++ Webpage/JS/ShapeRecognition_server.js
@@ -1,12 +1,7 @@
-var pattern_length = 5;     // increment this number as the level increases
-var num_shapes = 6; 	    // this number must be 2 or greater (to have diferrent sequence variations)
-var lvl = 1;
-
-var patterns = {}; // an empty JS object, later it's going to store the pattern for each end-user
-
 var express = require('express');
 var app = express();
-var idCounter = 0;
+
+var scores = {}; // an empty JS object, later it's going to store the score for the top ten end-users
 
 app.post('/post', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -16,43 +11,28 @@ app.post('/post', (req, res) => {
     var z = JSON.parse(req.query['data']);
 
     // check if the request action is generatePattern
-    if (z['action'] == "generatePattern") {
-        //generate a pattern for this user
-        idCounter++;
-        var nameID = z['name'] + idCounter;
-        generatePattern(nameID);
+    if (z['action'] == "update_leaderboard") {
+        //generate a record for this user
+        var nameID = z['name'];
+        var score = z['score'];
+        update_leaderboard(nameID, score);
+        // sends response of user's score after loss at the game
         var jsontext = JSON.stringify({
-            'action': 'generatePattern',
+            'action': 'update_leaderboard',
             'nameID': nameID,
-            'msg': 'New pattern generated!!!'
+            'score': score,
+            'msg': 'Leaderboard updated'
         });
         console.log(jsontext);
-        console.log(patterns);
         // send the response while including the JSON text		
         res.send(jsontext);
-    } else if (z['action'] == "evaluate") {
-        //evaluate the attempt_pattern for this user
-        var [num_match, num_wrong, num_score]
-            = evaluate(codes[z['name']], z['attempt_pattern']);
-        
-        var win = false;
-        var answer = [];
-        if ((num_match == pattern_length) || (lvl % 3 == 0))
-            pattern_length++    // For every lvl reached that is divisible by 3 the pattern length is incremented by 1
-        if (num_match == pattern_length)
-            answer = patterns[z['name']];
-            win = true;
-            lvl++;
-        
-        /* the response will have 6 parts: request action, whether won or not, number of exact matches,
-        number of wrong shapes, the current score of the client and the answer if the game ended */
+    } else if (z['action'] == "display_leaderboard") {
+        display_leaderboard(nameID, highscore);
+        /* the response will have 3 parts: request action, the name of the top 10 scores, the current top 10 scores of all clients (NOT COMPLETE)*/
         var jsontext = JSON.stringify({
-            'action': 'evaluate',
-            'win': win,
-            'num_match': num_match,
-            'num_wrong': num_wrong,
-            'num_score': num_score,
-            'pattern': answer
+            'action': 'display_leaderboard',
+            'highscore': highscore,
+            'msg': 'Leaderboard displayed'
         });
         console.log(jsontext);
         res.send(jsontext);
@@ -62,50 +42,24 @@ app.post('/post', (req, res) => {
 }).listen(3000);
 console.log("Server is running!");
 
-/*
- * Evaluate the client's attempting pattern
- * @param pattern is the server generated pattern for this client
- * @param attempt_pattern is the client attempted pattern in this request
- * @param time is the elapsed time before the client attempted pattern was inputted
- * @return num_match, num_wrong, num_score
- */
-function evaluate(pattern, attempt_pattern, time) {
-    var num_match = 0;   		 // number of exact matches, a good shape in a right spot
-    var num_wrong = 0;			 // number of wrong shapes
-    var num_score = 0;           // time * lvl multiplier * difficulty elapsed (Used to calculate score)
+function update_leaderboard(clientName, score) {
+    //update leaderboard csv file with client's highest score (NOT COMPLETE)
+    /*
+    const fs = require('fs')
+    const csv = require('csv-parser')
+    const filename = 'leaderboard.csv';
 
-    //calculate the result
-    for (var i = 0; i < pattern.length; i++) {
-        if (pattern[i] == attempt_pattern[i]) num_match++;
-        else if (pattern_code.includes(pattern[i])) {
-            num_score += time;  // the score is currently the time elapse (We can incorporate the lvl into algorithim after)
+    fs.writeFile(filename, extractAsCSV(users), err => {
+        if (err) {
+            console.log('Error writing to csv file', err);
+        } else {
+            console.log(`saved as ${filename}`);
         }
-        else num_wrong++;
-    }
-
-    return [num_match, num_wrong, num_score];
-
+    });
+    */
 }
 
-/*
- * Generate a Pattern for this client
- * @param clientName is this client name
- */
-function generatePattern(clientName) {
-    //generate pattern
-    var pattern = [];
-    while (pattern.length < pattern_length) {
-        var id = Math.floor(Math.random() * num_shapes) + 1;
-        if (pattern.indexOf(id) === -1) pattern.push(id);
-    }
-    //store the pattern for this client
-    patterns[clientName] = pattern;
+function display_leaderboard() {
+    //display leadboard to the client
+
 }
-
-/*
-As of Nov 21st the only changes made to Lab09's version of this server code is
-the changing of variables according to our game, determining a basic algorithim
-for a score (score = time elapsed), and a difficulty modifier per lvl.
-
-Code must be added to client side js file to reciprocate current server code
-*/
