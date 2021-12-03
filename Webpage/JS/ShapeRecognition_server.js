094aa55d7abebb4b32742224943e913310fca0b2
yuaefimoff <92180203+yuaefimoff@users.noreply.github.com>
2021-12-03 17:23:22 -0500

    Update ShapeRecognition_server.js

--- Webpage/JS/ShapeRecognition_server.js
+++ Webpage/JS/ShapeRecognition_server.js
@@ -1,65 +1,94 @@
+var fs = require('fs');
+var codes = {};
 var express = require('express');
 var app = express();
+var idCounter = 0;
+var scoreNum = 0;
 
-var scores = {}; // an empty JS object, later it's going to store the score for the top ten end-users
-
-app.post('/post', (req, res) => {
+app.get('/getuser', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    console.log("New express client");
-    console.log("Received: ");
-    console.log(JSON.parse(req.query['data']));
-    var z = JSON.parse(req.query['data']);
+    console.log("Getting users.... ");
+    var out = readfile();
+    res.send(out);
+    console.log("Users listed");
+});
 
-    // check if the request action is generatePattern
-    if (z['action'] == "update_leaderboard") {
-        //generate a record for this user
-        var nameID = z['name'];
-        var score = z['score'];
-        update_leaderboard(nameID, score);
-        // sends response of user's score after loss at the game
-        var jsontext = JSON.stringify({
-            'action': 'update_leaderboard',
-            'nameID': nameID,
-            'score': score,
-            'msg': 'Leaderboard updated'
-        });
-        console.log(jsontext);
-        // send the response while including the JSON text		
-        res.send(jsontext);
-    } else if (z['action'] == "display_leaderboard") {
-        display_leaderboard(nameID, highscore);
-        /* the response will have 3 parts: request action, the name of the top 10 scores, the current top 10 scores of all clients (NOT COMPLETE)*/
-        var jsontext = JSON.stringify({
-            'action': 'display_leaderboard',
-            'highscore': highscore,
-            'msg': 'Leaderboard displayed'
-        });
-        console.log(jsontext);
-        res.send(jsontext);
-    } else {
-        res.send(JSON.stringify({ 'msg': 'error!!!' }));
+function readfile() {
+    var filedata;//Stores text file
+    var fileerror;//Stores error
+    try {//Read give text file
+        filedata = fs.readFileSync('users/alice.txt', 'utf8');
+        console.log(filedata);
+        //console.log(name);
+        //console.log(scoreNum);
+    } catch (err) {
+        fileerror = err;
+        console.error(err);
     }
-}).listen(3000);
-console.log("Server is running!");
+    return filedata;
+}
 
-function update_leaderboard(clientName, score) {
-    //update leaderboard csv file with client's highest score (NOT COMPLETE)
-    /*
-    const fs = require('fs')
-    const csv = require('csv-parser')
-    const filename = 'leaderboard.csv';
-
-    fs.writeFile(filename, extractAsCSV(users), err => {
-        if (err) {
-            console.log('Error writing to csv file', err);
-        } else {
-            console.log(`saved as ${filename}`);
+app.get('/setuser', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    var name = req.query['name'];
+    var score = req.query['score'];
+    var content = '';
+    var filedata = readfile();
+    console.log('Setting users....');
+    console.log(filedata);
+    var usersjson;
+    try {
+        usersjson = JSON.parse(filedata);
+    } catch (err) {
+        console.log(err);
+        usersjson = {};
+    }
+    console.log(usersjson);
+    if (usersjson.hasOwnProperty('users')) {
+        console.log('JSON file OK!');
+        var found = false;
+        console.log(usersjson.users);
+        for (var i = 0; i < usersjson.users.length; i++) {
+            var obj = usersjson.users[i];
+            if (name == obj.name) {
+                found = true;
+                obj.score = parseInt(score);
+                break;
+            }
         }
+        if (!found) {
+            usersjson.users.push({ "name": name, "score": parseInt(score) });
+        }
+        content = JSON.stringify(usersjson);
+    } else {
+        console.log('JSON file broke!');
+        content = `{"users":[{"name":"` + name + `","score":` + score + `}]}`;
+        console.log(content);
+    }
+    fs.writeFile('users/alice.txt', content,
+        function (err) { if (err) { return console.log(err); } });
+    res.send(content);
+});
+
+app.get('/setsequence', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    var highlightTimes = req.query['highlight'];
+    var arrSeq = req.query['storearray'];
+    var arrlength = req.query['arrlength'];
+    console.log("Creating a new sequence.... ");
+    var someSequence = [];
+    for (var i = 0; i < highlightTimes; i++) {
+        var someNum = Math.floor(Math.random() * arrlength);
+        someSequence.push(someNum + 1);
+    }
+    arrSeq = someSequence;
+    console.log("Sequence code: " + arrSeq);
+    var sequencejson = JSON.stringify({
+        'sequence': arrSeq
     });
-    */
-}
+    console.log("Sequence created");
+    console.log(sequencejson);
+    res.send(sequencejson);
+});
 
-function display_leaderboard() {
-    //display leadboard to the client
-
-}
+app.listen(3000);
